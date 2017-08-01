6c15b863a4dd550958344a5042e5cef6d643e128
Alana Schwendler <alanaschwendler27@gmail.com>
2017-08-01 10:01:19 -0300

    Arena toda preenchida

--- snake.asm
+++ snake.asm
@@ -43,7 +43,6 @@
 .macro deslocamentoLinha %qt
 	move $a0, %qt
 	ori $a1, $zero, 32
-	#sll $a1, $a1, 2
 	mult $a0, $a1
 	mflo $a2
 	deslocamentoPixel $a2
@@ -58,7 +57,7 @@
 	
 	#cores do jogo
 	corFundo: .word 0xacc4f6
-	corComida: .word 0x82acf2
+	corComida: .word 0xd41a1a
 	corCobra: .word 0x000080080 
 	corBorda: .word 0x000080080 
 	
@@ -73,6 +72,11 @@
 	posComX: .half 5
 	posComY: .half 5
 	
+	#movimentos
+	cima: .asciiz "w"
+	baixo: .asciiz "s"
+	esquerda: .asciiz "a"
+	direita: .asciiz "d"
 .text
 
 MAIN: 
@@ -103,7 +107,12 @@ ARENA:				#subrotina que preenche a arena
 	nop
 	
 	push $s0
-	jal BORDA
+	jal BORDA		#subrotina para preencher as bordas da arena
+	nop
+	pop $s0
+	
+	push $s0	
+	jal COMIDA		#subrotina para preencher a comida
 	nop
 	pop $s0
 	
@@ -153,27 +162,40 @@ BORDA:				#subrotina para preencher as bordas da tela
 	
 	jal BORDA_BASE		#vai pro preenchimento da borda da base
 	nop
+	
+	pop $s0			#desempilha s0
+	push $s0		#empilha s0
+	
+	jal BORDA_ESQUERDA	#subrotina que pinta a borda da esquerda
+	nop
+	
+	pop $s0			#desempilha s0
+	push $s0		#empilha s0
+	
+	jal BORDA_DIREITA	#subrotina que imprime a borda da direita
+	nop
+	
 	pop $s0			#desempilha o s0
 	pop $ra			#desempilha o ra
 	jr $ra			#volta pra subrotina chamadora
 	nop
 	
-BORDA_TOPO:
+BORDA_TOPO:			#inicio borda topo com inicialização de registradores
 	push $ra
 	or $t0, $zero, 0	#zera o contador
-	j LOOP_BORDA_TOPO
+	j LOOP_BORDA_TOPO	#vai pro loop
 	nop
 	
-LOOP_BORDA_TOPO:		#preenchimento da borda
+LOOP_BORDA_TOPO:		#preenchimento da borda com loop
 	
 	sw $s4, 0($s0)		#pinta o pixel com o valor da cor de borda
 	add $s0, $s0, 0x4	#soma um pixel na base
 	add $t0, $t0, 1		#incrementa o contador
 		
-	beq $t0, 32, END_BORDA_TOPO
+	beq $t0, 32, END_BORDA_TOPO	#se o contador chegar em 32, termina o loop. O valor é 32 porque são 512 pixeis divididos por 16 (tamanho do pixel) = 32
 	nop
 	
-	j LOOP_BORDA_TOPO
+	j LOOP_BORDA_TOPO	#caso o contador não seja 32 ainda, continua no loop
 	nop
 	
 END_BORDA_TOPO:
@@ -190,15 +212,65 @@ BORDA_BASE:
 	nop
 
 LOOP_BORDA_BASE:
-	sw $s4, 0($s0)
-	add $s0, $s0, 0x4
-	add $t0, $t0, 1
-	beq $t0, 32, END_BORDA_BASE
+	sw $s4, 0($s0)		#pinta a borda com a cor de borda
+	add $s0, $s0, 0x4	#incrementa um pixel na base de endereçamento
+	add $t0, $t0, 1		#incrementa o contador
+	beq $t0, 32, END_BORDA_BASE	#se o contador é 32, termina o loop
 	nop
-	j LOOP_BORDA_BASE
+	j LOOP_BORDA_BASE	#caso contrário, continua no loop
 	nop
 	
-END_BORDA_BASE:
+END_BORDA_BASE:			#termina o loop de preenchimento da base
 	pop $ra
 	jr $ra
 	nop
+	
+BORDA_ESQUERDA:
+	push $ra		#empilha o ra
+	or $t0, $zero, 0 	#zera o contador
+	ori $t1, $zero, 32	#t1 recebe 32 pra deslocar esta quantidade de pixeis e pintar só a borda
+	j LOOP_BORDA_ESQUERDA	#vai pro loop de preencher a borda da esquerda
+	nop
+	
+LOOP_BORDA_ESQUERDA:		#loop de preenchimento da borda esquerda
+	deslocamentoPixel $t1	#manda o macro fazer o deslocamento de 32 pixeis (equivalente a uma linha)
+	sw $s4, 0($s0)		#pinta um pixel (primeiro de cada linha)
+	add $t0, $t0, 1		#incrementa o contador
+	beq $t0, 32, END_LOOP_BORDA_ESQUERDA	#caso o contador tenha chegado em 32, ou seja, imprimiu o primeiro pixel de todas as linhas, termina
+	nop	
+	j LOOP_BORDA_ESQUERDA	#caso contrário, continua no loop até imprimir de todas as linhas
+	nop
+	
+END_LOOP_BORDA_ESQUERDA:	#termina o loop de preenchimento da borda esquerda
+	pop $ra
+	jr $ra
+	nop
+	
+BORDA_DIREITA:
+	push $ra		#empilha o ra
+	or $t0, $zero, 0 	#zera o contador
+	ori $t1, $zero, 32	#t1 recebe 32 pra deslocar esta quantidade de pixeis e pintar só a borda
+	j LOOP_BORDA_DIREITA	#vai pro loop de preencher a borda da direita
+	nop
+	
+LOOP_BORDA_DIREITA:		#loop para preencher a borda da direita
+	deslocamentoPixel $t1	#faz o deslocamento de 32 bits e vai preencher uma coluna inteira, ao final
+	sw $s4, 124($s0)	#pega cada pixel da coluna e desloca 124, até fiacar a coluna toda na borda direita
+	add $t0, $t0, 1		#incrementa 
+	beq $t0, 32, END_LOOP_BORDA_DIREITA	#caso já tenha feito preenchimento para todas as linhas, termina o loop
+	nop
+	j LOOP_BORDA_DIREITA	#caso contrário, continua no loop
+	nop
+
+END_LOOP_BORDA_DIREITA:
+	pop $ra			#desempilha o ra
+	jr $ra			#volta pra subrotina chamadora
+	nop
+	
+COMIDA:
+	push $ra
+	sw $s2, 1096($s0)
+	pop $ra
+	jr $ra
+	nop
+	
